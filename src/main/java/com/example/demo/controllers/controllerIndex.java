ac17a3d8c6a6403c10d26dd9c8a1d314f8da7ab9
elraton88 <elraton88@elraton88-PC>
2018-02-23 03:34:09 +0100

    commit pojos

--- src/main/java/com/example/demo/controllers/controllerIndex.java
+++ src/main/java/com/example/demo/controllers/controllerIndex.java
@@ -1,13 +1,13 @@
-package controllers;
+package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class controllerIndex {
+public class controllerIndex { //CONTROLADOR DE LA VISTA PRINCIPAL
 
-	
+	//LANZA LA VISTA PRINCIPAL
 	@RequestMapping("/vistaIndex")
 	public ModelAndView vistaLogin() {
 		return new ModelAndView("index");
